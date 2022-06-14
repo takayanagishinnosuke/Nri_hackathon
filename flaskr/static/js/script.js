60b819ac5ba6240fbb59225665021b17a2fc22ce
t_shinnosuke <97178451+takayanagishinnosuke@users.noreply.github.com>
2022-06-15 02:52:58 +0900

    topにjsonデータを追加

---
+++ flaskr/static/js/script.js
@@ -0,0 +1,1 @@
+console.log(kannon_data)
--- flaskr/top.py
+++ flaskr/top.py
@@ -1,13 +1,19 @@
+from base64 import encode
 import functools
-from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
+from flask import Blueprint, flash, g, jsonify, redirect, render_template, request, session, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 import pandas as pd
+import json
 
 bp = Blueprint('top', __name__,)
 
 @bp.route('/top', methods=('GET', 'POST'))
 def index():
   db = get_db()
- 
-  return render_template('top/index.html')
+  kannondata = pd.read_sql_query('SELECT name, temple_name, address, latitude, longitude FROM kannondata',db)
+  kanon_df = kannondata.dropna(how='any')
+  kannon_dict = kanon_df.to_dict(orient='index')
+  # js_kannon_data = json.dumps(kannon_dict, ensure_ascii=False)
+  
+  return render_template('top/index.html', kannon_dict=kannon_dict)
